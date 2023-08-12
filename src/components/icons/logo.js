52152ff6d08c2b0ebb2e58f292b536fb21dc523c
elliechen5 <142103830+elliechen5@users.noreply.github.com>
2023-08-12 08:00:07 -0700

    Add files via upload

---
+++ src/components/icons/logo.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const IconLogo = () => (
+  <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
+    <title>Logo</title>
+    <g transform="translate(-8.000000, -2.000000)">
+      <g transform="translate(11.000000, 5.000000)">
+        <polygon
+          id="Shape"
+          stroke="currentColor"
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          points="39 0 0 22 0 67 39 90 78 68 78 23"
+        />
+        <path
+          d="M28.541667,30.3 V61 H45.691667 V56.5 H33.591667 V47.5 H44.691667 V43 H33.591667 V34.8 H45.691667 V30.3 H28.541667z"
+          fill="currentColor"
+        />
+      </g>
+    </g>
+  </svg>
+);
+
+export default IconLogo;
